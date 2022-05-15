9fdf5817ed5397183231b4e0f7c5dc274014affa
Carolina <carolinaruo@gmail.com>
2022-05-15 11:27:38 -0300

    MovieCard

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,13 +1,27 @@
 import Pagination from "components/Pagination";
-import MovieStars from "components/MovieStars";
-import MovieScore from "components/MovieScore";
+import MovieCard from "components/MovieCard";
 
 function Listing() {
     return (
         <div><Pagination />
-            <MovieScore />
-            <MovieStars /></div>
 
+            <div className="container">
+                <div className="row">
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 }
 
