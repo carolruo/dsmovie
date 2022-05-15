2290aa2179b5a718926da9325903449b832519f0
Carolina <carolinaruo@gmail.com>
2022-05-15 18:54:20 -0300

    useState useEffect

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -2,12 +2,17 @@ import Pagination from "components/Pagination";
 import MovieCard from "components/MovieCard";
 import axios from "axios";
 import { BASE_URL } from "utils/requests";
+import { useState } from "react";
+import { MoviePage } from "types/movie";
 
 function Listing() {
 
+
+    const [pageNumber, setPageNumber] = useState(0);
+
     //forma errada
     axios.get(`${BASE_URL}/movies?size=12&page=0`).then(response => {
-        console.log(response.data);
+        const data = response.data as MoviePage;
     });
 
     return (
